415b29ac883544b801f725bf09dc37d87d68ab14
ribeirosamuel96 <ribeirosamuel96@gmail.com>
2022-07-17 22:56:33 -0300

    Implementando a lixeira e algumas mudanças

--- main_imap.py
+++ main_imap.py
@@ -6,30 +6,64 @@ def main():
     while logado == False:
         login()
         logado = True
-    
-    numMensa = numeroTotalMensagens() #selecionando o inbox e pegando o número de mensagens
-    
+
+    #selecionando o inbox
+    resposta_servidor = seleciona_mailbox("INBOX")
+
+    #pegando o número de mensagens
+    numMensa = numeroTotalMensagens(resposta_servidor) 
     while True:
         print("SELECIONE A OPÇÃO:".center(70,"_"))
         opcao = int(input("1-Ver E-mails    2-Ver Lixeira   3-Fazer Logout".center(69," ")))
         if opcao == 1:
-            print("="*70)
-            listarCabecalho(numMensa)
-            print("-"*70)
-            opcaoCabe = int(input("1-Abrir E-mail 2-Voltar: "))     
-            while True:
-                if opcaoCabe == 1:
-                    abrirEmail(numMensa)
-                    print("-"*70)
-                    opcaoAberto = int(input("\n1-Responder 2-Voltar: "))
-                    if opcaoAberto == 2:
-                        break
-                if opcaoCabe == 2:
+            sistema_visualizacao_mailbox("INBOX")
+
+        else:
+            if(opcao == 2):
+                sistema_de_lixeira()
+            else:
+                if  opcao == 3:
+                    logout()
                     break
-        
-        if  opcao == 3:
-                logout()
+
+
+def sistema_visualizacao_mailbox(nome_mailbox):
+    resposta_servidor = seleciona_mailbox(nome_mailbox)
+    numero_mensagens = numeroTotalMensagens(resposta_servidor)
+    lista_uids = uids(numero_mensagens)
+    while True:
+        print("="*70)
+        print(" "*35 + nome_mailbox)
+        print("="*70)
+       
+        listar_cabecalhos(lista_uids)
+        print("-"*70) 
+        opcao_escolhida = int(input("Escolha um e-mail para abrir ou -1 para voltar: "))
+      
+        if opcao_escolhida >=1 and opcao_escolhida < len(lista_uids):
+            visualizar_email(lista_uids[opcao_escolhida -1][4])
+            print("-"*70)
+            opcao_aberto = int(input("\n1-Responder 2-Excluir 3-Voltar: "))
+            # if opcao_aberto == 3:
+            #     break
+        else:
+            if opcao_escolhida == -1:
                 break
+            print("\nNúmero de email inválido.")
+        
+
+
+def sistema_de_lixeira():
+    ##visualizar_lixeira()
+    if(not verifica_existencia_mailbox("TRASH")):
+        if(cria_mailbox("TRASH")):
+            print("Lixeira criada.".center(75," "))
+   
+    sistema_visualizacao_mailbox("TRASH")
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- modulo_imap.py
+++ modulo_imap.py
@@ -1,5 +1,8 @@
 import socket
 import getpass
+import re
+
+
 
 
 #criando a conexão TCP com o servidor usando a porta do IMAP
@@ -17,7 +20,6 @@ else:
 
 #Realizando autenticação do usuário
 def login():
-
     login = input("Entre com seu e-mail: ")
     senha = getpass.getpass(prompt='Password: ', stream=None)
     login = login.split("@")
@@ -30,15 +32,56 @@ def login():
         print("Falha ao logar")
         return False
 
-#função que retorna o número total de mensagens existentes
-def numeroTotalMensagens():
-    tcp.send("2 SELECT inbox\r\n".encode())
+
+def seleciona_mailbox(email_folder_name):
+    tcp.send("2 SELECT {}\r\n".format(email_folder_name).encode())
     recv = (tcp.recv(1024))
     recv = recv.decode('utf-8').split("\r\n")
-    numeroMesangens = recv[2]
-    numeroMesangens = list(numeroMesangens)
-    numeroMesangens = int(numeroMesangens[2]) #pegando o número total de mensagens 
-    return numeroMesangens
+    return recv
+
+
+
+#função que retorna o número total de mensagens existentes
+def numeroTotalMensagens(resposta_servidor):
+
+    for i in range(0, len(resposta_servidor)):
+        if "EXISTS" in resposta_servidor[i]:
+            break
+    
+    if(i < len(resposta_servidor)):
+        numbers_regex = re.compile(r'\d+(?:\.\d+)?')
+        numero_mensagens = numbers_regex.findall(resposta_servidor[i])
+        numero_mensagens = int(numero_mensagens[0]) #pegando o número total de mensagens 
+        return numero_mensagens
+    
+    return 0
+
+def fecha_mailbox_atual():
+    tcp.send("30 close \r\n".encode())
+    recv = (tcp.recv(1024))
+    return "OK" in recv.decode(('utf-8'))
+    
+
+
+def verifica_existencia_mailbox(nome_folder):
+    tcp.send("20 list \"\" \"*\" \r\n".encode())
+    recv = (tcp.recv(1024))
+    recv = recv.decode('utf-8').split("\r\n")
+    for i in range(0, len(recv)):
+        if(nome_folder in recv[i]):
+            return True
+    return False
+
+def cria_mailbox(nome_folder):
+    tcp.send("21 create {} \r\n".format(nome_folder).encode())
+    recv = (tcp.recv(1024))
+    return "OK" in recv.decode(('utf-8'))  
+
+
+
+
+
+
 
 #função que cria uma matriz com os uids, flags e status das mensagem
 def uids(tamanho): 
@@ -73,6 +116,21 @@ def listarCabecalho(tamanho):
             print(recv[i])
     print()
 
+def listar_cabecalhos(uid):
+    for cont in range(len(uid)-1):
+        tcp.send(f"5 UID fetch {uid[cont][4]} (body[header.fields (from to subject date)])\r\n".encode())
+        recv = tcp.recv(2048)
+        recv = recv.decode("utf-8")
+        recv = recv.split("\r\n")
+        if  (uid[cont][6]) == "(\Seen" or (uid[cont][6]) == "(\Seen))":
+            print(f"Email {uid[cont][1]} Visualizado".center(70, '_'))
+        else:
+            print(f"Email {uid[cont][1]} Não Visualizado".center(70, '_'))
+        for i in range(1,5):
+            print(recv[i])
+    print()
+    
+
 def abrirEmail(tamanho):
     uid = uids(tamanho)
     numeroDoEmail = input("Forneça o número do email que deseja abrir: ")
@@ -87,6 +145,14 @@ def abrirEmail(tamanho):
             
     print("Número que corresponde ao email não foi encontrado")  
     return abrirEmail(tamanho)
+
+
+def visualizar_email(email_uid):
+    tcp.send(f"6 UID fetch {email_uid} (UID RFC822.SIZE BODY.PEEK[])\r\n".encode())
+    #tcp.send(f"6 fetch {uid[cont][4]} body[text]\r\n".encode())
+    recv = tcp.recv(1024)
+    recv = recv.decode("utf-8")
+    return print("\n",recv)    
 
     
 
